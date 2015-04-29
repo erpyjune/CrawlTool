1029e3c0b1e073dc6a6b807f8aa48cbaf1e7c0cd
erpyjune <thanks.lot@gmail.com>
2015-04-30 01:29:08 +0900

    add source.

--- src/main/java/com/dao/SearchDataDAOService.java
+++ src/main/java/com/dao/SearchDataDAOService.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
  */
 @Repository
 public class SearchDataDAOService implements SearchDataDAO {
+
     @Autowired
     private SqlSession sqlSession;
 
--- src/main/java/com/erpy/crawltool/CrawlController.java
+++ src/main/java/com/erpy/crawltool/CrawlController.java
@@ -1,18 +1,29 @@
 package com.erpy.crawltool;
 
+import com.dao.SearchData;
+import com.dao.SearchDataDAOService;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 /**
  * Created by baeonejune on 15. 4. 29..
  */
 @Controller
 public class CrawlController {
+
+    @Autowired
+    private SearchDataDAOService searchDataDAOService;
+
     @RequestMapping(value = "/main", method = RequestMethod.GET)
-    public ModelAndView printWelcome(ModelAndView model) {
+    public ModelAndView getSearchListAll(ModelAndView model) {
+        List<SearchData> searchDataList = searchDataDAOService.getAllSearchData();
         model.addObject("message", "Hello world!");
         model.setViewName("hello");
         return model;
